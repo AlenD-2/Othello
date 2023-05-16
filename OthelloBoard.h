ff2f36dab20508fd0ea1658d302ceb94bad35729
Ali Nadi <alinadi.cp@gmail.com>
2023-05-16 17:36:48 +0330

    add & change: add Position structure and move Piece enum inside the class

--- OthelloBoard.h
+++ OthelloBoard.h
@@ -7,17 +7,29 @@
 
 namespace Othello {
 
-typedef std::array<std::array<int, 8>, 8> board_t;
+const uint16_t BOARD_SIZE = 8;
+typedef std::array<std::array<int, BOARD_SIZE>, BOARD_SIZE> board_t;
 
-enum Piece
-{
-    empty,
-    white,
-    black
-};
 
 class OthelloBoard
 {
+public:
+    enum Piece
+    {
+        empty,
+        white,
+        black
+    };
+    struct Position
+    {
+        int row = -1;
+        int column = -1;
+        bool isEmpty()
+        {
+            return (row == -1 || column == -1);
+        }
+    };
+
 public:
     OthelloBoard();
     void setPointTo(int x, int y, Piece piece);
