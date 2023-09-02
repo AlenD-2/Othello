fcc4a06a793cac94b2bab961750c8b91da8e7240
Ali Nadi <alinadi.cp@gmail.com>
2023-09-03 00:06:41 +0330

    Add: PlayersModel class connected to Player class for first test

--- PlayersModel.h
+++ PlayersModel.h
@@ -10,22 +10,31 @@ class PlayersModel : public QObject
 
 public:
     Q_PROPERTY(QString player1Name READ player1Name /*WRITE setPlayer1Name*/ NOTIFY player1NameChanged FINAL);
+    Q_PROPERTY(int player1Move READ player1Move WRITE setPlayer1Move NOTIFY player1MoveChanged FINAL);
+
+    Q_INVOKABLE void sendBoardToPlayer1(QVector<int> board);
 
 public:
     explicit PlayersModel(QObject *parent = nullptr);
     ~PlayersModel();
 
 signals:
-    void player1NameChanged();
     void programFinished();
+    void player1ReadyToMove(QString board);
+
+    void player1NameChanged();
+    void player1MoveChanged();
 
 public slots:
     void onPlayer1Ready();
+    void onPlayer1NewMove(QString move);
 
     QString player1Name();
+    int player1Move();
 
 private:
     Player _player1;
+    int _player1LastMove;
 
 };
 
