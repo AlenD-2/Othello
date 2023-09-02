9133fd378d7b6c54746bb7ee1d2982b9da87b253
Ali Nadi <alinadi.cp@gmail.com>
2023-09-02 21:00:35 +0330

    Add: class Player added with a basic event loop

--- player.cpp
+++ player.cpp
@@ -1,8 +1,43 @@
-
-#include "player.h"
+#include "Player.h"
+#include <QDir>
 
 Player::Player()
 {
-
+    this->moveToThread(&_playerThread);
+    connect(&_playerThread, &QThread::started, this, &Player::readPlayerName);
+    _playerThread.start();
 }
 
+Player::~Player()
+{
+    _playerThread.exit();
+}
+
+void Player::readPlayerName()
+{
+    // initialize Player process (allocate memmory and start process
+    initProcess();
+
+    _playerProcess->waitForReadyRead();
+    auto name = _playerProcess->readAllStandardOutput();
+    qDebug()<<"player name = " << name;
+    emit playerReady(name);
+}
+
+void Player::killProcess()
+{
+    _playerProcess->kill();
+    _playerProcess->waitForFinished(10000);
+    qDebug()<<_playerProcess->isOpen();
+}
+
+void Player::initProcess()
+{
+    _playerProcess = std::make_unique<QProcess>();
+    _playerProcess->setProgram(QDir::currentPath()+"/p1.exe");
+    _playerProcess->start();
+    if(!_playerProcess->waitForStarted())
+    {
+        qDebug()<<"Error: can't start the process";
+    }
+}
--- player.h
+++ player.h
@@ -1,12 +1,32 @@
-
 #pragma once
 
+#include <QObject>
+#include <QThread>
+#include <QProcess>
 
+#include <memory>
 
-
-class Player
+class Player : public QObject
 {
+    Q_OBJECT
 public:
-    Player();
+    explicit Player();
+    ~Player();
+
+signals:
+    void playerReady(QString name);
+    void readyRead(int index);
+
+public slots:
+    void readPlayerName();
+    void killProcess();
+
+private:
+
+    QThread _playerThread;
+    std::unique_ptr<QProcess> _playerProcess;
+
+    //functions
+    void initProcess();
 };
 
