8e5837a5cb1c9b036c4ce83d0ad411ad64ef748b
Ali Nadi <alinadi.cp@gmail.com>
2023-09-06 19:20:03 +0330

    revert PlayerModel class codes

--- PlayersModel.cpp
+++ PlayersModel.cpp
@@ -1,23 +1,11 @@
 #include "PlayersModel.h"
 #include "qdebug.h"
 
-void PlayersModel::sendBoardToPlayer1(QVector<int> board)
-{
-    QString strBoard{""};
-    for(const auto& i : board)
-    {
-        strBoard += QString::number(i);
-    }
-    emit player1ReadyToMove(strBoard);
-}
-
 PlayersModel::PlayersModel(QObject *parent)
     : QObject{parent}
 {
     connect(this, &PlayersModel::programFinished, &_player1, &Player::killProcess);
     connect(&_player1, &Player::playerReady, this, &PlayersModel::onPlayer1Ready);
-    connect(this, &PlayersModel::player1ReadyToMove, &_player1, &Player::writeBoard);
-    connect(&_player1, &Player::readyReadMove, this, &PlayersModel::onPlayer1NewMove);
 }
 
 PlayersModel::~PlayersModel()
@@ -32,18 +20,7 @@ void PlayersModel::onPlayer1Ready()
     emit player1NameChanged();
 }
 
-void PlayersModel::onPlayer1NewMove(QString move)
-{
-    _player1LastMove = move.toInt();
-    emit player1MoveChanged();
-}
-
 QString PlayersModel::player1Name()
 {
     return _player1.getName();
-}
-
-int PlayersModel::player1Move()
-{
-    return _player1LastMove;
 }
--- PlayersModel.h
+++ PlayersModel.h
@@ -10,31 +10,32 @@ class PlayersModel : public QObject
 
 public:
     Q_PROPERTY(QString player1Name READ player1Name /*WRITE setPlayer1Name*/ NOTIFY player1NameChanged FINAL);
+<<<<<<< HEAD
     Q_PROPERTY(int player1Move READ player1Move /*WRITE setPlayer1Move*/ NOTIFY player1MoveChanged FINAL);
 
     Q_INVOKABLE void sendBoardToPlayer1(QVector<int> board);
+=======
+>>>>>>> parent of fcc4a06 (Add: PlayersModel class connected to Player class for first test)
 
 public:
     explicit PlayersModel(QObject *parent = nullptr);
     ~PlayersModel();
 
 signals:
-    void programFinished();
-    void player1ReadyToMove(QString board);
-
     void player1NameChanged();
-    void player1MoveChanged();
+    void programFinished();
 
 public slots:
     void onPlayer1Ready();
-    void onPlayer1NewMove(QString move);
 
     QString player1Name();
-    int player1Move();
 
 private:
     Player _player1;
+<<<<<<< HEAD
     int _player1LastMove=0;
+=======
+>>>>>>> parent of fcc4a06 (Add: PlayersModel class connected to Player class for first test)
 
 };
 
